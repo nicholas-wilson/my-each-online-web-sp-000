da5b228fdf9d1f39f31aac37176f200f58e24a7f
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 18:24:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
 def my_each(array)
-  if block_given?
-    counter = 0
+  counter = 0
 
-    while counter < array.length
-      yield
-      counter += 1
-    end
-    array
-  else
-    puts "No block was given"
+  while counter < array.length
+    yield
+    counter += 1
   end
+  array
 end
