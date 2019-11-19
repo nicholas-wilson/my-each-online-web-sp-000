36211352e3bbcb3dc338d145358fa56d70121ada
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 18:23:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    counter = 0
+
+    while counter < array.length
+      yield
+      counter += 1
+    end
+    array
+  else
+    puts "No block was given"
+  end
 end
